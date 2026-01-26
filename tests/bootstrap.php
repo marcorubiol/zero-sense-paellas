d8d4af7d1d5c8a69b3d0a58a57f52dff4b59cd99
marcorubiol <marcorubiol@gmail.com>
2026-01-26 14:43:20 +0100

    chore: remove dev-only files

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-if (!defined('ABSPATH')) {
-    define('ABSPATH', sys_get_temp_dir() . '/wordpress/');
-}
-
-Brain\Monkey\setUp();
