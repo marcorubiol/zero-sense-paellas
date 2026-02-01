3adf982908af64fe3aa70477f08fe6bb40724b93
marcorubiol <marcorubiol@gmail.com>
2026-02-01 21:41:33 +0100

    feat: register EmailContentMetabox and CustomerPreferencesMetabox components in EventManagement Bootstrap

--- src/ZeroSense/Features/WooCommerce/EventManagement/Bootstrap.php
+++ src/ZeroSense/Features/WooCommerce/EventManagement/Bootstrap.php
@@ -2,6 +2,8 @@
 namespace ZeroSense\Features\WooCommerce\EventManagement;
 
 use ZeroSense\Features\WooCommerce\EventManagement\Components\EventDetailsMetabox;
+use ZeroSense\Features\WooCommerce\EventManagement\Components\EmailContentMetabox;
+use ZeroSense\Features\WooCommerce\EventManagement\Components\CustomerPreferencesMetabox;
 use ZeroSense\Features\WooCommerce\EventManagement\Components\DataExposer;
 use ZeroSense\Features\WooCommerce\EventManagement\Components\ServiceAreaAdminColumns;
 
@@ -13,6 +15,8 @@ class Bootstrap
     public function boot(): void
     {
         (new EventDetailsMetabox())->register();
+        (new EmailContentMetabox())->register();
+        (new CustomerPreferencesMetabox())->register();
         (new DataExposer())->register();
         (new ServiceAreaAdminColumns())->register();
     }
