af36de6810bed38a98abe5f06493834449d5a155
marcorubiol <marcorubiol@gmail.com>
2026-02-16 16:05:34 +0100

    fix: correct typo in jQuery variable reference from 'thdis' to 'this' in Classic order edit screen metabox title modification

--- src/ZeroSense/Features/WooCommerce/Admin/AdminSectionTitles.php
+++ src/ZeroSense/Features/WooCommerce/Admin/AdminSectionTitles.php
@@ -68,7 +68,7 @@ class AdminSectionTitles implements FeatureInterface
             jQuery(document).ready(function($) {
                 function changeMetaboxTitles() {
                     $('#order_data h3').each(function() {
-                        var elem = $(this);
+                        var elem = $(thdis);
                         var html = elem.html();
                         
                         if (html.indexOf('Billing') === 0 || html.indexOf('Facturación') === 0) {
