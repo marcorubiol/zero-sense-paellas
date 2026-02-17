53925e07c6423a0e3ec86cd536a06dcf9215b274
marcorubiol <marcorubiol@gmail.com>
2026-02-17 09:21:35 +0100

    refactor: improve stock admin table styling with alternating row backgrounds, enhanced category headers with uppercase styling, and reposition save button as fixed bottom-right element, bump asset version to 1.0.3

--- src/ZeroSense/Features/WooCommerce/EventManagement/Inventory/Components/StockAdminPage.php
+++ src/ZeroSense/Features/WooCommerce/EventManagement/Inventory/Components/StockAdminPage.php
@@ -46,7 +46,7 @@ class StockAdminPage
             'zs-stock-admin',
             $baseUrl . 'src/ZeroSense/Features/WooCommerce/EventManagement/Inventory/assets/css/stock-admin.css',
             [],
-            '1.0.2'
+            '1.0.3'
         );
         
         wp_enqueue_script(
