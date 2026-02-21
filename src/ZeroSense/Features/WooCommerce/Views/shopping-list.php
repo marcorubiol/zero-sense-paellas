527857e0d2a3ee2b108cdfaca63723f9b956e29e
marcorubiol <marcorubiol@gmail.com>
2026-02-21 12:16:00 +0100

    refactor: add outline style to reset button and fix Catalan translation from 'Empezar' to 'Començar'

--- src/ZeroSense/Features/WooCommerce/Views/shopping-list.php
+++ src/ZeroSense/Features/WooCommerce/Views/shopping-list.php
@@ -42,7 +42,7 @@ declare(strict_types=1);
             </div>
             <div class="zs-sl__filter-group zs-sl__filter-group--full zs-sl__filter-actions">
                 <button type="button" class="btn--primary zs-sl__search-btn" id="zs-sl-search"><?php esc_html_e('Cercar comandes', 'zero-sense'); ?></button>
-                <button type="button" class="btn--neutral zs-sl__reset-btn" id="zs-sl-reset"><?php esc_html_e('Empezar de nou', 'zero-sense'); ?></button>
+                <button type="button" class="btn--neutral btn--outline zs-sl__reset-btn" id="zs-sl-reset"><?php esc_html_e('Començar de nou', 'zero-sense'); ?></button>
             </div>
         </div>
     </div>
