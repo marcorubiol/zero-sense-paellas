4bb074713dd9757f4e8230d5ada1c10ec3ed4f26
marcorubiol <marcorubiol@gmail.com>
2026-02-25 06:11:08 +0100

    chore: include vendor/ in repo for easier deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'paellasencasa/zero-sense-v3',
+        'pretty_version' => '3.0.0',
+        'version' => '3.0.0.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'paellasencasa/zero-sense-v3' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
