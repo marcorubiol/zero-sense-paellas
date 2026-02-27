3a916e9f9865c871738f34d1bbaa86ecf149b283
marcorubiol <marcorubiol@gmail.com>
2026-02-27 06:45:57 +0100

    refactor: simplify section title styling by removing absolute positioning and tag badges

--- assets/js/admin-section-titles.js
+++ assets/js/admin-section-titles.js
@@ -3,20 +3,16 @@ jQuery(document).ready(function ($) {
         $('#order_data h3').each(function () {
             var elem = $(this);
 
-            if (elem.data('zs-processed')) {
+            if (elem.next('.zs-subtitle').length) {
                 return;
             }
 
             var firstWord = elem.contents().first().text().trim().split(/\s+/)[0];
 
             if (firstWord === 'Billing' || firstWord === 'Facturación') {
-                elem.text('Client');
-                elem.append('<span class="zs-wc-tag">Billing</span>');
-                elem.data('zs-processed', true);
+                elem.after('<div class="zs-subtitle zs-subtitle-client">Client</div>');
             } else if (firstWord === 'Shipping' || firstWord === 'Envío') {
-                elem.text('Wedding Planner - Venue');
-                elem.append('<span class="zs-wc-tag">Shipping</span>');
-                elem.data('zs-processed', true);
+                elem.after('<div class="zs-subtitle zs-subtitle-venue">Wedding Planner - Venue</div>');
             }
         });
 
