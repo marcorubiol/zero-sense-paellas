e113caa71402bfc581adadd24e40b8049de6872a
marcorubiol <marcorubiol@gmail.com>
2026-02-27 06:57:14 +0100

    refactor: adjust badge positioning to top-right corner and add auto-generated badge class

--- assets/js/admin-section-titles.js
+++ assets/js/admin-section-titles.js
@@ -9,14 +9,14 @@ jQuery(document).ready(function ($) {
                     elem.text('Client');
                     elem.data('zs-modified', true);
                     elem.parent().css('position', 'relative');
-                    elem.after('<span class="zs-badge zs-subtitle-billing">Billing</span>');
+                    elem.after('<span class="zs-badge zs-badge-autozs-subtitle-billing">Billing</span>');
                 }
             } else if (firstWord === 'Shipping' || firstWord === 'Envío') {
                 if (!elem.data('zs-modified')) {
                     elem.text('Wedding Planner - Venue');
                     elem.data('zs-modified', true);
                     elem.parent().css('position', 'relative');
-                    elem.after('<span class="zs-badge zs-subtitle-shipping">Shipping</span>');
+                    elem.after('<span class="zs-badge zs-badge-auto zs-subtitle-shipping">Shipping</span>');
                 }
             }
         });
